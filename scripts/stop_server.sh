9d162e335810e50a952af8085d80bcc711464542
Jay Bhatt <jpb@narola.email>
2020-12-14 16:13:43 +0530

    Added Script folder with scripts

---
+++ scripts/stop_server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+isExistApp=`pgrep apache2`
+if [[ -n  $isExistApp ]]; then
+   service apache2 stop
+fi
+isExistApp=`pgrep mysql`
+if [[ -n  $isExistApp ]]; then
+    service mysql stop
+fi
